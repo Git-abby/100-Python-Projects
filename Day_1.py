926fd905aed66cf1abc5cabd788dfbb1ef1c10f4
Git-abby <malekabrar56@gmail.com>
2024-10-09 16:17:20 -0400

    Day 01 - Print Statement

---
+++ Day_1.py
@@ -0,0 +1,5 @@
+print('''************************************
+Day 01 of 100 days of Python Project
+************************************''')
+
+print("Hi, This is Mohmmadabrar\nWelcome to the 100-Days of Python Projects\nWe'll cover basics to intermediate project in the next 100 days")
