3b96b8a35f0f402994e4ca2fdf9731c0aec77b80
Git-abby <malekabrar56@gmail.com>
2024-10-12 13:41:51 -0400

    Day - 03 Odd/Even Checker

---
+++ Day_3.py
@@ -0,0 +1,12 @@
+print('''************************************
+Day 02 of 100 days of Python Project
+************************************''')
+
+print("Welcome to ODD-EVEN CHECKER")
+
+number = int(input("Enter the number? "))
+
+if number%2 == 0:
+    print("It's E-V-E-N")
+else:
+    print("O-D-D Number!")
